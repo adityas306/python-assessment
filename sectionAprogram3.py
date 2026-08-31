97abe3770f70904fcc72d70af2bce65445e24eb5
sadityabhardwaj <sadityabhardwaj@gmail.com>
2026-08-31 11:05:20 +0530

    Added Codes

---
+++ sectionAprogram3.py
@@ -0,0 +1,18 @@
+# 3.	Explain the difference between for loop and while loop. When would you prefer each? [3 Marks]
+# for loop
+for i in range(1 , 5):
+    print(i , end=" ")
+print()
+
+list1 = [1 , 2 , 4, 5]
+for i in list1:
+    print(i , end=" , ")
+print()
+
+
+#while Loop
+i = 1
+while(i <= 5):
+    print(i)
+    i += 1
+
